3f41f86d6dd76e794d37e6111b00266faa0ea1d4
AaronMuseo <aaron.museo@strathmore.edu>
2025-09-04 16:36:58 +0300

    implemented a class

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-print "<h1>Hello BBIT";
-print "<p> Today is " . date("l") . "</p>";
+class structure{
+    
+    public function heading(){
+        echo "<h1>Welcome to BBIT E</h1>";
+    }
+    public function footer(){
+        echo "<footer>&copy;" . date("Y") . "BBIT E. All rights reserved. </footer>";
+    }
+}
+
---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'index.php';
+
+//include the class file
+
+
+//creating an instance
+$page = new structure();
+
+//call the methods
+$page->heading();
+$page->footer();
